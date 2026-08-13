dd18ab12904598021bf0fae0a7a3683d1703eb4e
Syed Ifham <syedifham300@gmail.com>
2026-08-13 20:01:11 +0530

    test: hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import Hero from "./Hero.jsx";
+
 export default function App() {
   return (
-   <>
-
-   </>
+    <>
+      <main className="relative min-h-screen w-screen overflow-x-hidden">
+        <Hero/>
+      </main>
+    </>
   );
 }
+
+{/*<div*/}
+{/*  className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">*/}
+
+{/*  <div onClick={handleMiniVdClick} className="origin-center">*/}
+
+{/*    <video*/}
+{/*      src={getVideoSrc(currentIndex + 1)}*/}
+{/*      ref={nextVideoRef}>*/}
+
+
+{/*    </video>*/}
+{/*  </div>*/}
